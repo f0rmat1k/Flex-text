8d6a34254e5e711ae420fe826d0c72c4573ebc63
Anton Grischenko <f0rmat1k@yandex-team.ru>
2014-05-29 15:36:19 +0600

    first alpha release

--- flex-text.js
+++ flex-text.js
@@ -1,45 +1,83 @@
 ;(function(window, document){
     'use strict';
-
+    var self;
     function bindEvents(){
-        var self = this;
         window.addEventListener('resize', function(){
-
+            makeFlexText.call(self);
         }, false);
     }
 
-    function getBlocksNodes(blocks){
-        var _blocks = [];
+    bindEvents();
 
-        switch (typeof blocks) {
+    function getBlockNode(block){
+        var _block;
+
+        switch (typeof block) {
             case 'string':
-                _blocks = document.querySelectorAll(blocks);
+                _block = document.querySelector(block);
                 break;
             case 'object':
-                if (blocks.length && blocks[0].nodeType > 0) {
-                    _blocks = [].slice.call(blocks);
-                } else if (blocks.nodeType > 0) {
-                    _blocks.push(blocks);
+                if (block.nodeType > 0) {
+                    _block = block;
                 } else {
                     try { console.log('FlexText: wrong block type'); } catch (e) {}
                 }
                 break;
         }
 
-        this.blocks = _blocks;
+        this._block = _block;
     }
 
-    function getParams(){
+    function makeFlexText() {
+        var node = this._block.cloneNode(true),
+            text = document.createElement('div'),
+            checkContainer = document.createElement('div'),
+            resultFs = this._currentFs || this._baseFs;
 
+        node.style.width = this._block.clientWidth + 'px';
+        node.style.height = this._block.clientHeight + 'px';
+        text.innerHTML = node.innerHTML;
+        node.innerHTML = '';
+        node.appendChild(text);
+
+        checkContainer.style.position = 'fixed';
+        checkContainer.style.visibility = 'hidden';
+        checkContainer.style.zIndex = -10;
+        checkContainer.style.left = '500px';
+        checkContainer.appendChild(node);
+
+        document.body.appendChild(checkContainer);
+
+        if (text.clientHeight > node.clientHeight) {
+            while(text.clientHeight > node.clientHeight){
+                resultFs -= 1;
+                text.style.fontSize = resultFs + 'px';
+            }
+        } else {
+            while(text.clientHeight < node.clientHeight){
+                resultFs += 1;
+                text.style.fontSize = resultFs + 'px';
+            }
+            resultFs -= 1;
+        }
+
+        this._currentFs = resultFs;
+        this._block.style.fontSize = resultFs + 'px';
+        checkContainer.remove();
+    }
+
+    function collectParams(){
+        this._baseFs = parseInt(window.getComputedStyle(this._block, null).getPropertyValue('font-size'), 10);
+        this._height = this._block.offsetHeight;
     }
 
     bindEvents();
 
-    function FlexText(blocks){
-        getBlocksNodes.call(this, blocks);
-
-        bindEvents.call(this);
-        getParams.call(this);
+    function FlexText(block){
+        self = this;
+        getBlockNode.call(this, block);
+        collectParams.call(this);
+        makeFlexText.call(this);
     }
 
     window.FlexText = FlexText;
