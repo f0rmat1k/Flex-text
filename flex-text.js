64915ab3ad5e5d3c7a7d64da3c1aff8540993f3f
Anton Grischenko <f0rmat1k@yandex-team.ru>
2014-05-31 16:00:45 +0600

    fix 4 padding work

--- flex-text.js
+++ flex-text.js
@@ -1,13 +1,13 @@
-;(function(window, document){
+var FlexText = (function(window, document){
     'use strict';
     var self;
-    function bindEvents(){
-        window.addEventListener('resize', function(){
-            makeFlexText.call(self);
-        }, false);
-    }
+        function bindEvents(){
+            window.addEventListener('resize', function(){
+                makeFlexText.call(self);
+            }, false);
+        }
 
-    bindEvents();
+        bindEvents();
 
     function getBlockNode(block){
         var _block;
@@ -34,8 +34,11 @@
             checkContainer = document.createElement('div'),
             resultFs = this._currentFs || this._baseFs;
 
-        node.style.width = this._block.clientWidth + 'px';
-        node.style.height = this._block.clientHeight + 'px';
+//            node.style.width = this._block.clientWidth + 'px';
+        node.style.width = window.getComputedStyle(this._block, null).getPropertyValue('width');
+//            node.style.height = this._block.clientHeight + 'px';
+        node.style.height = window.getComputedStyle(this._block, null).getPropertyValue('height');
+
         text.innerHTML = node.innerHTML;
         node.innerHTML = '';
         node.appendChild(text);
@@ -48,13 +51,18 @@
 
         document.body.appendChild(checkContainer);
 
-        if (text.clientHeight > node.clientHeight) {
-            while(text.clientHeight > node.clientHeight){
+//            var nodeHeight = Math.floor(+ window.getComputedStyle(node, null).getPropertyValue('height').replace('px', ''));
+        var nodeHeight = + window.getComputedStyle(node, null).getPropertyValue('height').replace('px', '');
+
+//            console.log(window.getComputedStyle(node, null).getPropertyValue('height'));
+
+        if ((+ window.getComputedStyle(text, null).getPropertyValue('height').replace('px', '')) > nodeHeight) {
+            while((+ window.getComputedStyle(text, null).getPropertyValue('height').replace('px', '')) > nodeHeight){
                 resultFs -= 1;
                 text.style.fontSize = resultFs + 'px';
             }
-        } else {
-            while(text.clientHeight < node.clientHeight){
+        } else if ((+ window.getComputedStyle(text, null).getPropertyValue('height').replace('px', '')) < nodeHeight) {
+            while((+ window.getComputedStyle(text, null).getPropertyValue('height').replace('px', '')) < nodeHeight){
                 resultFs += 1;
                 text.style.fontSize = resultFs + 'px';
             }
@@ -68,10 +76,9 @@
 
     function collectParams(){
         this._baseFs = parseInt(window.getComputedStyle(this._block, null).getPropertyValue('font-size'), 10);
-        this._height = this._block.offsetHeight;
+//            this._height = this._block.clientHeight;
+//            this._height = window.getComputedStyle(this._block, null).getPropertyValue('height');
     }
-
-    bindEvents();
 
     function FlexText(block){
         self = this;
@@ -80,5 +87,5 @@
         makeFlexText.call(this);
     }
 
-    window.FlexText = FlexText;
+    return FlexText;
 })(window, document);
