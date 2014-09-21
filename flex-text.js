49a64b5ce0191219ca8bb124269a2d4e05fa7896
Anton Grischenko <f0rmat1k@yandex-team.ru>
2014-09-22 02:31:12 +0600

    Добавлена поддердка min, max шрифтов

--- flex-text.js
+++ flex-text.js
@@ -1,7 +1,6 @@
 var FlexText = (function(window, document){
     'use strict';
-    var self,
-        flexTextBlocks = [];
+    var flexTextBlocks = [];
 
     function bindEvents(){
         window.addEventListener('resize', onWindowResize__flextText, false);
@@ -19,14 +18,16 @@ var FlexText = (function(window, document){
         var _block;
 
         switch (typeof block) {
+            // string with css-selector
             case 'string':
                 _block = document.querySelector(block);
                 break;
             case 'object':
+                // DOM-node
                 if (block.nodeType > 0) {
                     _block = block;
                 } else {
-                    try { console.log('FlexText: wrong block type'); } catch (e) {}
+                    throw new Error('FlexText: wrong block type');
                 }
                 break;
         }
@@ -40,9 +41,7 @@ var FlexText = (function(window, document){
             checkContainer = document.createElement('div'),
             resultFs = this._currentFs || this._baseFs;
 
-//            node.style.width = this._block.clientWidth + 'px';
         node.style.width = window.getComputedStyle(this._block, null).getPropertyValue('width');
-//            node.style.height = this._block.clientHeight + 'px';
         node.style.height = window.getComputedStyle(this._block, null).getPropertyValue('height');
 
         text.innerHTML = node.innerHTML;
@@ -51,43 +50,62 @@ var FlexText = (function(window, document){
 
         checkContainer.style.position = 'fixed';
         checkContainer.style.visibility = 'hidden';
-        checkContainer.style.zIndex = -10;
-        checkContainer.style.left = '500px';
+        checkContainer.style.zIndex = -9999;
         checkContainer.appendChild(node);
 
         document.body.appendChild(checkContainer);
-
-//            var nodeHeight = Math.floor(+ window.getComputedStyle(node, null).getPropertyValue('height').replace('px', ''));
         var nodeHeight = + window.getComputedStyle(node, null).getPropertyValue('height').replace('px', '');
 
-//            console.log(window.getComputedStyle(node, null).getPropertyValue('height'));
+        if (window.getComputedStyle(text, null).getPropertyValue('height').replace('px', '') > nodeHeight) {
+            while(window.getComputedStyle(text, null).getPropertyValue('height').replace('px', '') > nodeHeight){
+                if (resultFs > this._options.min) {
+                    resultFs -= this._options.step;
+                    text.style.fontSize = resultFs + 'px';
+                } else {
+                    this._options.onOverflow.call(this, {
+                        block: this._block,
+                        fontSize: resultFs
+                    });
 
-        if ((+ window.getComputedStyle(text, null).getPropertyValue('height').replace('px', '')) > nodeHeight) {
-            while((+ window.getComputedStyle(text, null).getPropertyValue('height').replace('px', '')) > nodeHeight){
-                resultFs -= 1;
-                text.style.fontSize = resultFs + 'px';
+                    break;
+                }
             }
-        } else if ((+ window.getComputedStyle(text, null).getPropertyValue('height').replace('px', '')) < nodeHeight) {
-            while((+ window.getComputedStyle(text, null).getPropertyValue('height').replace('px', '')) < nodeHeight){
-                resultFs += 1;
-                text.style.fontSize = resultFs + 'px';
+        } else if (window.getComputedStyle(text, null).getPropertyValue('height').replace('px', '') < nodeHeight) {
+            while(window.getComputedStyle(text, null).getPropertyValue('height').replace('px', '') < nodeHeight){
+                if (resultFs < this._options.max) {
+                    resultFs += this._options.step;
+                    text.style.fontSize = resultFs + 'px';
+                } else {
+                    break;
+                }
             }
-            resultFs -= 1;
+
+            resultFs -= this._options.step;
         }
-
+        resultFs > this._options.max ? resultFs = this._options.max : null;
         this._currentFs = resultFs;
         this._block.style.fontSize = resultFs + 'px';
-        checkContainer.remove();
+        checkContainer.parentNode.removeChild(checkContainer);
     }
 
     function collectParams(){
         this._baseFs = parseInt(window.getComputedStyle(this._block, null).getPropertyValue('font-size'), 10);
-//            this._height = this._block.clientHeight;
-//            this._height = window.getComputedStyle(this._block, null).getPropertyValue('height');
     }
 
-    function FlexText(block){
-        self = this;
+    function FlexText(block, options){
+        var resultOptions = {
+            min: 5,
+            max: 999,
+            step: 0.1,
+            onOverflow: function(){}
+        };
+
+        //extend ops
+        Object.keys(options).forEach(function(prop){
+            resultOptions[prop] = options[prop];
+        });
+
+        this._options = resultOptions;
         flexTextBlocks.push(this);
         getBlockNode.call(this, block);
         collectParams.call(this);
